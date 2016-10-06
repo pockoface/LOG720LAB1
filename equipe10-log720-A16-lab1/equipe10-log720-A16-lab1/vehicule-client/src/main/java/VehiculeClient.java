c0f16befa574a432738b7604514677714fa6664a
Zephyrux <zaher127@hotmail.com>
2016-10-06 13:18:33 -0400

    gg

--- equipe10-log720-A16-lab1/equipe10-log720-A16-lab1/vehicule-client/src/main/java/VehiculeClient.java
+++ equipe10-log720-A16-lab1/equipe10-log720-A16-lab1/vehicule-client/src/main/java/VehiculeClient.java
@@ -3,37 +3,78 @@ import java.util.Scanner;
 public class VehiculeClient {
 	
 	public static String selectedFileRecord = "";
-	public static String selectedManager = "";
-	
+	public static int selectedManager = 0;
+	public static Scanner enteredValue = new Scanner(System.in);
+
 	public static void main(String[] args) {
 		
 		System.out.println("Client du véhicule: ");
 		displayMainMenu();
+		System.out.println("Veuillez sélectionner un choix à l'aide d'un chiffre (1, 2 ou 3).");
 		mainMenu();
 		
 	}
 	
 	private static void displayMainMenu(){
-		System.out.println("A - Gestionnaire de réactions");
-		System.out.println("B - Gestionnaire des dossiers");
-		System.out.println("C - Gestionnaire des infractions");
-		System.out.println("Veuillez sélectionner un choix à l'aide d'une lettre.");
+		System.out.println("1 - Gestionnaire des dossiers");
+		System.out.println("2 - Gestionnaire des réactions");
+		System.out.println("3 - Gestionnaire des infractions");
 	}
 	
 	private static void mainMenu(){
 		
-		while(selectedManager != "A" || selectedManager != "B" ||  selectedManager != "C"){
-			System.out.println("Attention! Veuillez entrer un choix valide!");
-			selectedManager = managerSelector();
+		selectedManager = managerSelector();
+		
+		while(selectedManager != 1 && selectedManager != 2 && selectedManager != 3){
+			System.out.println("Entrée invalide! Veuillez sélectionner l'une des options suivantes:");
+			displayMainMenu();
+			selectedManager = managerSelector();	
 		}
 		
-		Scanner valeurEntree = new Scanner(System.in);
-		System.out.println(valeurEntree);
-	}
-	
-	private static String managerSelector(){
-		return "Test";
+		switch(selectedManager){
+		case 1:
+			displayFilesMenu();
+			enteredValue.close();
+			break;
+		case 2:
+			displayReactionsMenu();
+			enteredValue.close();
+			break;
+		case 3:
+			displayInfractionsMenu();
+			enteredValue.close();
+			break;
+		default:
+			System.out.println("Entrée invalide! Veuillez sélectionner l'une des options suivantes:");
+			displayMainMenu();
+			selectedManager = managerSelector();
+			break;
+		}
+		
 		
 	}
 	
+	private static void displayFilesMenu(){
+		System.out.println("DOSSIERS");
+	}
+	
+	private static void displayReactionsMenu(){
+		System.out.println("Gestionnaire des réactions");
+		System.out.println("1 - Liste des réactions");
+		System.out.println("2 - Ajouter des réactions à la liste des réactions");
+		System.out.println("3 - Ajouter des réactions au dossier sélectionné");
+	}
+	
+	private static void displayInfractionsMenu(){
+		System.out.println("INFRACTIONS");
+	}
+
+	private static int managerSelector(){
+		try{
+			int returnValue =Integer.parseInt(enteredValue.next());
+			return returnValue;
+		}catch(Exception e){
+			return 0;	
+		}
+	} 
 }
